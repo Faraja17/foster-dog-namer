21e4306680c4d8b17ec917d5e124216a8b5b25bd
faraja17 <farajakthompson@gmail.com>
2022-09-03 16:21:35 -0400

    update copyright

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,27 +4,28 @@ const Footer = () => {
     return (
         <>
             <div className='footer' id='footer'>
-            
+
                 <span className='connect'>
                     <a className="connect" href="https://www.soocial.com/dog-instagram-names/" >Instagram</a>
-                
+
                 </span>
-            
+
                 <span className='connect'>
                     <a className="connect" href="https://www.tiktok.com/tag/dognames?lang=en" >TikTok</a>
-                
+
                 </span>
 
                 <span className='connect'>
                     <a className="connect" href="https://www.youtube.com/results?search_query=Letting+Homeless+Puppies+Pick+Their+Name" >YouTube</a>
-                
+
                 </span>
-            
-            
-            <p className="copyright"> &copy; {new Date().getUTCFullYear()} Faraja Thompson. All rights reserved.</p>
-        </div>
+
+
+                <p className="copyright"> &copy; {new Date().getUTCFullYear()} Faraja Thompson</p>
+            </div>
         </>
 
-)};
+    )
+};
 
 export default Footer;
