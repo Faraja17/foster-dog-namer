ea751fed96ab24c4610b1b4e6c41a06b79b0e5af
faraja17 <farajakthompson@gmail.com>
2022-09-04 17:46:47 -0400

    update signup component

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -9,14 +9,11 @@ const Signup = () => {
        <>
             <div id="signup">
                 <form action="/signup" className="form-signin" method="POST">
-                    <img className="mb-4 dog-print" src="https://cdn.pixabay.com/photo/2012/04/18/00/06/print-36174_960_720.png" alt="" width="72" height="57" />
+                    <a href="https://dog-namer-app.herokuapp.com/"><img className="mb-4 dog-print" src="https://cdn.pixabay.com/photo/2012/04/18/00/06/print-36174_960_720.png" alt="" width="72" height="57" />
+                    </a>
                     <h1 className="h3 mb-3 fw-normal newsletter">Sign up for our newsletter!</h1>
-    
-                    <input type="text" name="fName" className="form-control top" placeholder="First Name" required autofocus />
-                    <input type="text" name="lName" className="form-control middle" placeholder="Last Name" required />
-                    <input type="email" name="email" className="form-control bottom" placeholder="Email" required />
-        
-                    <button className="w-100 btn btn-lg btn-primary" type="submit">Register</button>
+                    <a className="w-100 btn btn-lg btn-primary" href="https://dog-namer-app.herokuapp.com/" role="button">Subscribe</a>
+                    
                     {/* <p className="mt-5 mb-3 text-muted">&copy; 2022 Foster Dog Namer</p> */}
                 </form>
             </div>
